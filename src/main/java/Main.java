74613003b0cdcf9a8ab61415386a5016902244c4
find0ub7 <leon.watanabe@gmail.com>
2022-01-20 09:17:43 -0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.letscode.gradle.utils.ArrayUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(ArrayUtils.estaVazio(null));
+    }
+}
